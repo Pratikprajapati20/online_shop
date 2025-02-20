7f2f53125b326f2840359db21301bc17c325720d
Pratik Prajapati <prajapatipratik20@gmail.com>
2025-02-20 14:56:26 -0500

    ViteConfig:Update environment settings

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: './',
-    css: {
-      devSourcemap: false
-    }
+  plugins: [react()],
+  base: './',
+  css: {
+    devSourcemap: false
+  },
+  server: {
+    port: 3000
+  }
 })
